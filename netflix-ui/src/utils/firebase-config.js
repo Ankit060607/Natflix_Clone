7b5f83e52ff8e798d8fc8211123f36f60145b392
Ankit Kumar <132119585+Ankit060607@users.noreply.github.com>
2024-08-25 02:33:06 +0530

    final netflix clone with backend

--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -1,15 +1,21 @@
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1Hf0_rdWLBzDPJPcO9CJN4y6M6-EgKH4",
-  authDomain: "react-auth-6788d.firebaseapp.com",
-  projectId: "react-auth-6788d",
-  storageBucket: "react-auth-6788d.appspot.com",
-  messagingSenderId: "131797845021",
-  appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
-  measurementId: "G-VWPBR1NSLL",
+  apiKey: "AIzaSyCnM7RDYWZP4b055BDutxr4u41G_HkuvaU",
+  authDomain: "react-netflix-clone-e3274.firebaseapp.com",
+  projectId: "react-netflix-clone-e3274",
+  storageBucket: "react-netflix-clone-e3274.appspot.com",
+  messagingSenderId: "816451748079",
+  appId: "1:816451748079:web:dccee156960c1d50ae5b1a",
+  measurementId: "G-YF4W8XVW8L"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
